1ff3414d52c6ff7a7db53adcf2a35c5bc2d77cd6
Tim Pietrusky <timpietrusky@gmail.com>
2013-12-17 01:13:47 +0100

    Initial commit.

--- kittify.php
+++ kittify.php
@@ -4,24 +4,21 @@
  */
 /*
   Plugin Name: Kittify
-  Plugin URI: http://wordpress.org/plugins/kittify/
+  Plugin URI: https://github.com/bullgit/kittify
   Description: You really love your cat? Show it with Kittify and replace every image with a picture of a sweet kitty. 
   Author: Tim Pietrusky
   Version: 1.0
   Author URI: http://timpietrusky.com
 */
-?>
 
-<?php 
-
-  // Load the kittify.js
-  function kittify_script() {
-    if (!is_admin()) {
-      wp_register_script('kittify_js', plugins_url() . '/' . basename(dirname(__FILE__)) . '/kittify.js', array(), '1.0.0'); 
-      wp_enqueue_script('kittify_js'); 
-    }
+// Load the kittify.js
+function kittify_script() {
+  if (!is_admin()) {
+    wp_register_script('kittify_js', plugins_url() . '/' . basename(dirname(__FILE__)) . '/kittify.js', array(), '1.0.0'); 
+    wp_enqueue_script('kittify_js'); 
   }
+}
 
-  // Add the kittify.js to the footer
-  add_action('wp_footer', 'kittify_script');
+// Add the kittify.js to the footer
+add_action('wp_footer', 'kittify_script');
 ?>
